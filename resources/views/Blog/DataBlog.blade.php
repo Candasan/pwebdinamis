9651dfa286e26634ac235ed3a3612a2b6f9a4292
Candasan <monikafitria30@gmail.comgit>
2019-10-31 14:40:25 +0800

    update laman add blog dan data blog

--- resources/views/Blog/DataBlog.blade.php
+++ resources/views/Blog/DataBlog.blade.php
@@ -28,12 +28,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Starter Page</h1>
+            <h1 class="m-0 text-dark">Data Blog Page</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-
+              <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
+              <li class="breadcrumb-item active">Blog Data</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
